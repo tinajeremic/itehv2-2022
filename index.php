e8372a6fd16dedcc627764791b9c64ba5fe56440
Tina Jeremic <tinajeremic3@gmail.com>
2022-10-20 10:57:57 +0200

    ispravljanje greske

--- index.php
+++ index.php
@@ -1,4 +1,28 @@
-<?php
-echo "Hello world!"; 
-echo "<br> Moj prvi commit";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>FON: Zakazivanje kolokvijuma</title>
+
+</head>
+<body>
+    <div class="login-form">
+        <div class="main-div">
+            <form method="POST" action="#">
+                <div class="container">
+                    <label class="username">Korisnicko ime</label>
+                    <input type="text" name="username" class="form-control"  required>
+                    <br>
+                    <label for="password">Lozinka</label>
+                    <input type="password" name="password" class="form-control" required>
+                    <button type="submit" class="btn btn-primary" name="submit">Prijavi se</button>
+                </div>
+
+            </form>
+        </div>
+
+        
+    </div>
+</body>
+</html>
